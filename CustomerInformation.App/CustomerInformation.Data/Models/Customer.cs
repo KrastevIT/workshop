d2e494ebae2a9472396e3f4019c9bc730c5e1be2
KrastevIT <barcodebit000@gmai.com>
2021-01-30 11:33:09 +0200

    Add Data Model Customer

---
+++ CustomerInformation.App/CustomerInformation.Data/Models/Customer.cs
@@ -0,0 +1,13 @@
+﻿namespace CustomerInformation.Data
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string MiddleName { get; set; }
+
+        public string LastName { get; set; }
+    }
+}
